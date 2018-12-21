b2a4ddbc589f2e287b52fe328f060d9dd123e715
MaryLapko <mary.putilina@gmail.com>
2018-12-22 00:51:54 +0300

    first commit

---
+++ ReverseAword/ReverseAword/Practice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseAword
+{
+    public class Practice
+    {
+        public static void ReverseAwordFirstOption()
+        {
+            Console.WriteLine("Insert the word to reverse:");
+            var someWordToReverse = Console.ReadLine();
+            var someWordReversed = someWordToReverse.ToCharArray().Reverse();
+            Console.WriteLine("The reversed word is:" + new string(someWordReversed.ToArray()));
+        }
+        public static void ReverseAwordSecondOption()
+        {
+            Console.WriteLine("Insert the word to reverse:");
+            var someWordToReverse1 = Console.ReadLine();
+            var sb = new StringBuilder();
+            //В цикле мы начинаем читать элементы строки с конца.
+            for (int i = someWordToReverse1.Length - 1; i >= 0; i--)
+            {
+                sb.Append(someWordToReverse1[i]);
+            }
+            Console.WriteLine(sb.ToString()); //321cba
+        }
+    }
+}
---
+++ SimpleCalcIfElse/SimpleCalsIfElse/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simple_calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char again = 'y';
+            while (again == 'y')
+            {
+                double a;
+                double b;
+                double total;
+                char oper;
+                Console.WriteLine("Input specific number:");
+                a = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Input a specific operator:");
+
+                oper = Convert.ToChar(Console.ReadLine());
+
+                Console.WriteLine("Input another number:");
+                b = Convert.ToDouble(Console.ReadLine());
+
+                if (oper == '+')
+                {
+                    total = a + b;
+                    Console.WriteLine("Summ " + a + " and " + b + " equals " + total + ".");
+                }
+
+                else if (oper == '-')
+                {
+                    total = a - b;
+                    Console.WriteLine("Difference " + a + " and " + b + " equals " + total + ".");
+                }
+
+                else if (oper == '*')
+                {
+                    total = a * b;
+                    Console.WriteLine("Multiplication " + a + " on " + b + " equals " + total + ".");
+                }
+
+                else if (oper == '/')
+                {
+                    total = a / b;
+                    Console.WriteLine("Division " + a + " on " + b + " eqauals " + total + ".");
+                }
+                else
+                {
+                    Console.WriteLine("Unknown operator.");
+                }
+                Console.WriteLine("Do you want to continue using the calculator? (y/n)");
+                again = Convert.ToChar(Console.ReadLine());
+            }
+        }
+    }
+}
